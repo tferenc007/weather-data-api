5b9b853b92d990b3b537edfeed49d70857a5e8d0
Tomasz Ferenc <tomasz.ferenc@pb.com>
2024-04-15 17:01:11 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from os import name
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<word>")
+def about(word):
+    definition = word.upper()
+    return {"definition": definition, "word": word}
+if __name__ == "__main__":
+    app.run(debug=True, port=5000)
