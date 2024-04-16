102571d98c41adad51434f27caa51357c28b8161
Tomasz Ferenc <tomasz.ferenc@pb.com>
2024-04-16 10:41:38 +0200

    add definition to api

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import name
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -10,7 +11,9 @@ def home():
 
 @app.route("/api/v1/<word>")
 def about(word):
-    definition = word.upper()
+    df = pd.read_csv("data/dictionary.csv", sep=',')
+
+    definition = df.loc[df['word']==word]["definition"].squeeze()
     return {"definition": definition, "word": word}
 if __name__ == "__main__":
     app.run(debug=True, port=5000)
